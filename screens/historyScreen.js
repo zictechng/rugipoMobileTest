835e37b00ffd997be363c1f924b6bd322af4e8d0
Zictech Technologies <zictechng@gmail.com>
2023-09-13 18:30:02 +0100

    Updated

--- screens/historyScreen.js
+++ screens/historyScreen.js
@@ -122,7 +122,7 @@ const HistoryScreen = () => {
       setIsConnected(state.isConnected);
       if(state.isConnected === true) {
         setConnectionState(false);
-        console.log("Connected ", isConnected);
+        //console.log("Connected ", isConnected);
       }
       else if(state.isConnected === false) {
         setConnectionState(true);
