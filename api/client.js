0d4468aced0c3014176292201f8da211e0225139
Zictech Technologies <zictechng@gmail.com>
2023-09-25 13:25:27 +0100

    update

--- api/client.js
+++ api/client.js
@@ -6,6 +6,6 @@ import axios from "axios";
 
 //export default axios.create({baseURL: 'http://192.168.43.228:3500'})
 
-export default axios.create({baseURL: 'https://crane-cuff-links.cyclic.cloud/'})
+export default axios.create({baseURL: 'https://crane-cuff-links.cyclic.cloud'})
 
 //export default axios.create({baseURL: 'https://rugipo-finance-mobile-api.onrender.com'})
